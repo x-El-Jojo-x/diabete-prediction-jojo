eae0f3377ea84157fe0571fb5518ad22be0727a1
Jordan <jordan.doublet59292@gmail.com>
2022-07-27 14:14:37 +0200

    sklearn new version

--- app.py
+++ app.py
@@ -3,7 +3,30 @@ import pandas as pd
 import numpy as np
 import pickle
 from PIL import Image
+import base64
 
+
+#-------------image-Background----------------------------
+def get_base64(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def set_background(png_file):
+    bin_str = get_base64(png_file)
+    page_bg_img = '''
+    <style>
+    .stApp {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    opacity: 0.8;
+    }
+    </style>
+    ''' % bin_str
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+set_background('fond.jpg')
+#---------------End-Image-Background--------------------
 
 st.title('Welcome to Diabetes Prediction Application using Machine Learning Algorithms') 
 
